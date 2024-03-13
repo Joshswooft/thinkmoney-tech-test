b98b5cae54478781608237b2e4882f74d370d476
Josh Dando <dandobusiness@gmail.com>
2024-03-13 15:35:51 +0000

    feat(pricing): gets unit price of a product by its sku

--- pricing/pricing.go
+++ pricing/pricing.go
@@ -4,9 +4,13 @@ type sku = rune
 type pence = int
 type quantity = uint
 
+type PricingData struct {
+	UnitPrice pence
+}
+
 // exposing pricing config for convenience
 type SpecialPricing struct {
-	Config map[sku]interface{}
+	Config map[sku]PricingData
 }
 
 func (p *SpecialPricing) GetPrice(sku sku, quantity quantity) pence {
@@ -15,6 +19,11 @@ func (p *SpecialPricing) GetPrice(sku sku, quantity quantity) pence {
 		return 0
 	}
 
-	return 0
+	pricingData, exists := p.Config[sku]
+	if !exists {
+		return 0
+	}
+
+	return pricingData.UnitPrice
 
 }
--- pricing/pricing_test.go
+++ pricing/pricing_test.go
@@ -8,9 +8,10 @@ import (
 func TestSpecialPricing_GetPrice(t *testing.T) {
 
 	skuA := 'A'
+	skuB := 'B'
 
 	type fields struct {
-		pricing map[sku]interface{}
+		pricing map[sku]PricingData
 	}
 	type args struct {
 		sku      sku
@@ -27,6 +28,25 @@ func TestSpecialPricing_GetPrice(t *testing.T) {
 			fields: fields{pricing: nil},
 			args:   args{sku: skuA, quantity: 5},
 			want:   0,
+		},
+		{
+			name: "returns 0 pence for a sku that doesnt exist in the pricing rules",
+			fields: fields{
+				pricing: map[sku]PricingData{skuA: {UnitPrice: 50}},
+			},
+			args: args{
+				sku:      skuB,
+				quantity: 1,
+			},
+			want: 0,
+		},
+		{
+			name: "returns the price of the product - pineapple = 50p",
+			fields: fields{
+				pricing: map[sku]PricingData{skuA: {UnitPrice: 50}},
+			},
+			args: args{sku: skuA, quantity: 1},
+			want: 50,
 		},
 	}
 	for _, tt := range tests {
