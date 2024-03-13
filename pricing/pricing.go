a18cf4347fb76815e2d2bf48ef81e699ecb43876
Josh Dando <dandobusiness@gmail.com>
2024-03-13 15:37:47 +0000

    feat(pricing): add custom pricing calculations

--- pricing/pricing.go
+++ pricing/pricing.go
@@ -5,12 +5,49 @@ type pence = int
 type quantity = uint
 
 type PricingData struct {
-	UnitPrice pence
+	UnitPrice       pence
+	SpecialPrice    pence
+	SpecialQuantity quantity
+}
+
+func (p *PricingData) HasSpecialOffer() bool {
+	if p == nil {
+		return false
+	}
+	return p.SpecialPrice != 0 || p.SpecialQuantity != 0
 }
 
 // exposing pricing config for convenience
 type SpecialPricing struct {
 	Config map[sku]PricingData
+}
+
+func (p *SpecialPricing) calculatePrice(data PricingData, quantity quantity) pence {
+	if p == nil {
+		return 0
+	}
+
+	if data.HasSpecialOffer() {
+
+		specialQuantity := data.SpecialQuantity
+		if specialQuantity <= 0 {
+			specialQuantity = 1
+		}
+
+		// the number of items that have qualified for the special price
+		bundleQuantity := quantity / specialQuantity
+
+		// left over items that will have unit price applied
+		remainingQuantity := quantity % specialQuantity
+
+		bundlePrice := pence(bundleQuantity) * data.SpecialPrice
+		regularPrice := pence(remainingQuantity) * data.UnitPrice
+
+		return bundlePrice + pence(regularPrice)
+	}
+
+	return data.UnitPrice * pence(quantity)
+
 }
 
 func (p *SpecialPricing) GetPrice(sku sku, quantity quantity) pence {
@@ -24,6 +61,16 @@ func (p *SpecialPricing) GetPrice(sku sku, quantity quantity) pence {
 		return 0
 	}
 
-	return pricingData.UnitPrice
+	return p.calculatePrice(pricingData, quantity)
 
+}
+
+func (p *SpecialPricing) PriceExists(sku sku) bool {
+	if p == nil {
+		return false
+	}
+
+	_, exists := p.Config[sku]
+
+	return exists
 }
--- pricing/pricing_test.go
+++ pricing/pricing_test.go
@@ -48,6 +48,38 @@ func TestSpecialPricing_GetPrice(t *testing.T) {
 			args: args{sku: skuA, quantity: 1},
 			want: 50,
 		},
+		{
+			name: "buying 3 pineapples for 130p special offer met",
+			fields: fields{
+				pricing: map[sku]PricingData{skuA: {UnitPrice: 50, SpecialPrice: 130, SpecialQuantity: 3}},
+			},
+			args: args{sku: skuA, quantity: 3},
+			want: 130,
+		},
+		{
+			name: "buying more pineapples than the special quantity still applies the special price + the unit price",
+			fields: fields{
+				pricing: map[sku]PricingData{skuA: {UnitPrice: 50, SpecialPrice: 130, SpecialQuantity: 3}},
+			},
+			args: args{sku: skuA, quantity: 4},
+			want: 130 + 50,
+		},
+		{
+			name: "special price for this day replaces the unit price",
+			fields: fields{
+				pricing: map[sku]PricingData{skuA: {UnitPrice: 50, SpecialPrice: 35, SpecialQuantity: 0}},
+			},
+			args: args{sku: skuA, quantity: 2},
+			want: 35 + 35,
+		},
+		{
+			name: "Free item",
+			fields: fields{
+				pricing: map[sku]PricingData{skuA: {UnitPrice: 50, SpecialQuantity: 1}},
+			},
+			args: args{sku: skuA, quantity: 2},
+			want: 0,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
